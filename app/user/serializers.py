58b35d4dd0d519fdfe2de912f84ebd515cf8d611
Fernando Augusto Rodrigues de Araujo <adrafdev@MacBook-Pro-de-Fernando.local>
2020-03-27 21:52:04 -0300

    Create Token View

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,8 +1,11 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
+
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
@@ -14,3 +17,34 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return User.objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                email=email,
+                password=password,
+            )
+
+            if not user:
+                msg = _('Unable to authenticate with provided credentials')
+                raise serializers.ValidationError(msg, code='authentication')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,17 @@
-from rest_framework import generics
+from rest_framework import authentication, generics, permissions
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import AuthTokenSerializer, UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 
 class CreateUserView(generics.CreateAPIView):
+    """Createa a new user in the system"""
     serializer_class = UserSerializer
+
+
